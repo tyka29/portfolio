293b255bcf87c49488aa22b5a22400044295850a
mikl291 <tyka2923@gmail.com>
2023-07-18 18:05:32 +0100

    delete dotenv

--- js/qrcode.js
+++ js/qrcode.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 var firstName, lastName, email, phone, mobile, website, company, position, address, postalCode, city;
 var valeur;
 
@@ -67,41 +65,3 @@ function change(element) {
     qr.value = vCardData;
 }
 
-function shareByEmail() {
-    // Récupérer les valeurs des champs du formulaire
-    var formData = {
-      firstName: document.getElementById('firstName').value,
-      lastName: document.getElementById('lastName').value,
-      email: document.getElementById('email').value,
-      phone: document.getElementById('phone').value,
-      mobile: document.getElementById('mobile').value,
-      website: document.getElementById('website').value,
-      company: document.getElementById('company').value,
-      position: document.getElementById('position').value,
-      address: document.getElementById('address').value,
-      postalCode: document.getElementById('postalCode').value,
-      city: document.getElementById('city').value
-    };
-  
-    // Générer le contenu de l'e-mail
-    var emailContent = 'Bonjour,\n\nVoici mon QR code vCard et mes informations de contact :\n\n';
-    emailContent += 'Nom : ' + formData.lastName + ' ' + formData.firstName + '\n';
-    emailContent += 'E-mail : ' + formData.email + '\n';
-    emailContent += 'Téléphone : ' + formData.phone + '\n';
-    emailContent += 'Mobile : ' + formData.mobile + '\n';
-    emailContent += 'Site Web : ' + formData.website + '\n';
-    emailContent += 'Entreprise : ' + formData.company + '\n';
-    emailContent += 'Poste : ' + formData.position + '\n';
-    emailContent += 'Adresse : ' + formData.address + '\n';
-    emailContent += 'Code postal : ' + formData.postalCode + '\n';
-    emailContent += 'Ville : ' + formData.city + '\n';
-  
-    // Générer le lien du QR code
-    var qrCodeLink = 'Lien du QR code : ' + qr.toDataURL();
-  
-    // Générer le lien de partage par e-mail
-    var mailToLink = 'mailto:' + process.env.MAIL_ADRESS + '?subject=Partage de ma carte de visite&body=' + encodeURIComponent(emailContent + qrCodeLink);
-
-    // Ouvrir la fenêtre de composition d'e-mail avec les données pré-remplies
-    window.location.href = mailToLink;
-}
