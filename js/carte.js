2fbfed6f0989f62585b9154c893c7200d94abcc7
mikl291 <tyka2923@gmail.com>
2023-07-20 00:42:35 +0100

    update files

--- js/carte.js
+++ js/carte.js
@@ -1,80 +1,20 @@
-function QRcode() {
-    const form = document.getElementById("vcardForm");
-    const formData = new FormData(form);
-  
-    // Récupérer les valeurs saisies dans le formulaire
-    const firstName = formData.get("firstName") || "";
-    const lastName = formData.get("lastName") || "";
-    const email = formData.get("email") || "";
-    const phone = formData.get("phone") || "";
-    const mobile = formData.get("mobile") || "";
-    const website = formData.get("website") || "";
-    const company = formData.get("company") || "";
-    const position = formData.get("position") || "";
-    const address = formData.get("address") || "";
-    const postalCode = formData.get("postalCode") || "";
-    const city = formData.get("city") || "";
-  
-    // Vérifier si l'adresse e-mail est saisie
-    if (!email) {
-      // Afficher un message d'erreur ou effectuer une action appropriée
-      alert("Veuillez saisir une adresse e-mail.");
-      return; // Arrêter l'exécution de la fonction
-    }
-  
-    // Générer la vCard en utilisant les valeurs saisies
-    const vcard = `BEGIN:VCARD
-  VERSION:3.0
-  N:${lastName};${firstName}
-  FN:${firstName} ${lastName}
-  ORG:${company}
-  TITLE:${position}
-  ADR:;;${address};${city};${postalCode};
-  TEL;CELL:${phone}
-  TEL;CELL:${mobile}
-  EMAIL:${email}
-  URL:${website}
-  END:VCARD`;
-  
-    const QR = document.getElementById("qr");
-    QR.innerHTML = ""; // Effacer le contenu précédent du QR code
-  
-    const qrCode = new QRCodeStyling({
-      width: 200,
-      height: 200,
-      type: "png",
-      data: vcard,
-      margin: 0,
-    });
-  
-    qrCode.append(QR);
-  }
-  
-  document.getElementById("generateQR").addEventListener("click", (e) => {
-    e.preventDefault();
-    QRcode();
-  });
-  
-  document.getElementById("downloadButton").addEventListener("click", () => {
-    const qrCodeImage = document.querySelector("#qr img");
-  
-    // Vérifier si l'image du QR code existe
-    if (qrCodeImage) {
-      // Créer un lien de téléchargement
-      const link = document.createElement("a");
-      link.href = qrCodeImage.src;
-      link.download = "qr_code.png"; // Spécifier le nom du fichier à télécharger
-  
-      // Cliquez sur le lien pour déclencher le téléchargement
-      link.click();
+const carouselItems = document.querySelectorAll('.carousel-item');
+
+let currentSlide = 0;
+
+function showSlide() {
+  carouselItems.forEach((item, index) => {
+    if (index === currentSlide) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
     }
   });
-  
-  // Réinitialisation du formulaire
-  document.getElementById("clearForm").addEventListener("click", () => {
-    const form = document.getElementById("vcardForm");
-    form.reset();
-    const QR = document.getElementById("qr");
-    QR.innerHTML = "";
-  });
-  
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % carouselItems.length;
+  showSlide();
+}
+
+setInterval(nextSlide, 3000);
